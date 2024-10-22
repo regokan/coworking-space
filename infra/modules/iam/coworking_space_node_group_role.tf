95d5724562ab191c226b5dcb8654d022d3a3f33f
regokan <regokan@gmail.com>
2024-10-22 12:17:15 +0530

    fix: add EBS volume management permissions

--- infra/modules/iam/coworking_space_node_group_role.tf
+++ infra/modules/iam/coworking_space_node_group_role.tf
@@ -20,17 +20,46 @@ resource "aws_iam_role" "coworking_space_node_group_role" {
   }
 }
 
+# Attach EKS worker node policy
 resource "aws_iam_role_policy_attachment" "coworking_space_node_group_role_node_policy_attachment" {
   role       = aws_iam_role.coworking_space_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
+# Attach EKS CNI policy for networking
 resource "aws_iam_role_policy_attachment" "coworking_space_node_group_role_cni_policy_attachment" {
   role       = aws_iam_role.coworking_space_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
+# Attach ECR read-only policy for pulling container images
 resource "aws_iam_role_policy_attachment" "coworking_space_node_group_role_ec2_policy_attachment" {
   role       = aws_iam_role.coworking_space_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# Inline policy for managing EBS volumes
+resource "aws_iam_role_policy" "coworking_space_node_group_role_ebs_policy" {
+  role = aws_iam_role.coworking_space_node_group_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "ec2:CreateVolume",
+          "ec2:AttachVolume",
+          "ec2:DeleteVolume",
+          "ec2:DetachVolume",
+          "ec2:DescribeVolumes",
+          "ec2:DescribeVolumeStatus",
+          "ec2:DescribeSnapshots",
+          "ec2:CreateTags",
+          "ec2:DeleteTags"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
 }
